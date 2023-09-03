65d57b5e6612854d1ac904adc04bad61ea701260
Diego Cardenas <diegocardwr@gmail.com>
2023-09-03 18:47:27 -0500

    build: Add WebSocket server initializer

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-
+import { Database } from "./database/database";
+import { Server } from "socket.io";
+import ChatListeners from "./listeners/UserChatListener";
+import { MessageModel } from "./models/MessageModel";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function main(){
+    const database = new Database();
+    await database.connect();
 
+    const messageModel = new MessageModel(database);
+
+    const port = parseInt(process.env.PORT || "3000");
+
+    const io = new Server(port);
+
+    const chatNamespace = io.of("/chat");
+    chatNamespace.on('connection', (socket)=>{
+        console.log("conectado");
+        const {id_chat} = socket.handshake.query;
+        
+        new ChatListeners(messageModel, io, socket);
+    });
 }
 
 main();

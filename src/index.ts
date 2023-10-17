d57b3cbc6351b7e00359b3bdea65babec05796da
Diego Cardenas <diegocardwr@gmail.com>
2023-10-17 16:34:01 -0500

    chore: add listeners

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { Database } from "./database/database";
+import http from "http";    
 import { Server } from "socket.io";
 import ChatListeners from "./listeners/UserChatListener";
 import { MessageModel } from "./models/MessageModel";
 import RouteListener from "./listeners/RouteListener";
 import { RouteModel } from "./models/RouterModel";
+import LocationListeners from "./listeners/LocationListener";
 
 async function main(){
     const database = new Database();
@@ -16,8 +18,10 @@ async function main(){
     const routeModel = new RouteModel(database);
 
     const port = parseInt(process.env.PORT || "3000");
+    const host = process.env.HOST || "localhost";
 
-    const io = new Server(port);
+    const server = http.createServer();
+    const io = new Server(server);
 
     const chatNamespace = io.of("/chat");
     chatNamespace.on('connection', (socket)=>{
@@ -31,14 +35,10 @@ async function main(){
 
     const locationNamespace = io.of("/location");
     locationNamespace.on('connection', (socket) => {
-        const { region } = socket.handshake.query;
-        if(!region || typeof region != "string"){
-            socket.emit("error", "no hay una localizacion");
-            return;
-        }
-
-        socket.broadcast.to(`location::${region}`).emit("location:add")
-    })
+        new LocationListeners(io, socket);
+    });
+    
+    server.listen(port, host);
 }
 
 main();
